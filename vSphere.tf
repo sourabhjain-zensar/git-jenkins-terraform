726cf069a690ea0d951bc8b669a322790c4420cb
sourabhjain-zensar <sourabh.jain@zensar.com>
2016-08-23 18:58:34 +0530

    Create vSphere.tf

---
+++ vSphere.tf
@@ -0,0 +1,26 @@
+# Configure the VMware vSphere Provider
+provider "vsphere" {
+  user           = "administrator@vsphere.local"
+  password       = "********"
+  vsphere_server = "10.44.120.6"
+ # user = "root"
+ # password = "redhat10"
+ # vsphere_server = "10.44.120.60"
+  allow_unverified_ssl  = true
+}
+
+# Create a virtual machine within the folder
+resource "vsphere_virtual_machine" "prodapp" {
+  name   = "prodapp"
+  vcpu   = 2
+  memory = 4096
+
+  network_interface {
+    label = "App-pg"
+  }
+
+  disk {
+    size = "1"
+        datastore = "Datastore"
+  }
+}
