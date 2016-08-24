4622c104378fa4694841d4d56bcf1195e895355a
sourabhjain-zensar <sourabh.jain@zensar.com>
2016-08-24 14:08:59 +0530

    Update vSphere.tf

--- vSphere.tf
+++ vSphere.tf
@@ -1,7 +1,7 @@
 # Configure the VMware vSphere Provider
 provider "vsphere" {
   user           = "administrator@vsphere.local"
-  password       = "Redhat10"
+  password       = "Redhat10!"
   vsphere_server = "10.44.120.6"
  # user = "root"
  # password = "redhat10"
